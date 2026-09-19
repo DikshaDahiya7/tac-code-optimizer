490bdaa28180cc58ebe9fa35e93d32d175bd1279
DikshaDahiya7 <dikshadahiya051@gmail.com>
2026-09-20 04:55:03 +0530

    Fix formatting and remove unnecessary imports

--- src/optilens-main.jsx
+++ src/optilens-main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import OptilensCore from './OptilensCore.jsx'
-import './optilens-styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
